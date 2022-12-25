cccfa892ee8e4ba931f5cc2c3c24e14c7a084ea0
JheraldC <jcaceresap@unsa.edu.pe>
2022-12-25 21:01:17 +0000

    update

---
+++ js/myScript.js
@@ -0,0 +1,27 @@
+function ShowLogin(){
+
+}
+
+function DoLogin(){
+
+}
+
+function showRegister(){
+  
+}
+
+function doRegister(){
+
+}
+
+function doaddClient(){
+
+}
+
+function doremoveClient(){
+
+}
+
+function doeditClient(){
+
+}
