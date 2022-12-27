f0328a7e1526112adeaf1200f176293f722a9a25
jcaceresap <jcaceresap@unsa.edu.pe>
2022-12-27 17:09:12 -0500

    Agregando la recoleccion de datos en todos los datos del cliente

--- cgi-bin/listClient.pl
+++ cgi-bin/listClient.pl
@@ -4,6 +4,12 @@ use warnings;
 use CGI;
 use DBI;
 
+my @firstName;
+my @lastName;
+my @dni;
+my @country;
+my @isHere;
+
 my $q = CGI -> new;
 print $q->header('text/xml;charset=UTF-8');y
 
@@ -14,6 +20,63 @@ sub CheckClient {
   my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
   my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
-  my $sql = "SELECT title FROM Articles WHERE owner=?";
+  my $sql = "SELECT firstName FROM Clients";
   my $sth = $dbh->prepare($sql);
+  $sth->execute();
+  while (my @row = $sth->fetchrow_array){
+   $firstName[$i]=$row[0];
+  }
+  
+  my $sql = "SELECT lastName FROM Clients";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+  while (my @row = $sth->fetchrow_array){
+   $lastName[$i]=$row[0];
+  }
+
+  my $sql = "SELECT dni FROM Clients";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+  while (my @row = $sth->fetchrow_array){
+   $dni[$i]=$row[0];
+  }
+
+  my $sql = "SELECT country FROM Clients";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+  while (my @row = $sth->fetchrow_array){
+   $country[$i]=$row[0];
+  }
+
+  my $sql = "SELECT isHere FROM Clients";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+  while (my @row = $sth->fetchrow_array){
+   $isHere[$i]=$row[0];
+  }
+
+}
+
+sub showClient {
+  my @first = $_[0];
+  my @last = $_[1];
+  my @dni = $_[2];
+  my @country = $_[3];
+  my @isHere = $_[4]; 
+
+  my @body;
+
+  if(defined(@first)){
+    for(my $i = 0; $i<@first; $i++){
+      $body[$i] = <<XML;
+      <clients>
+        <firstname>$first[$i]</firstname>
+        <lasname>$las[$i]</lastname>
+        <dni>$dni[$i]</dni>
+        <country>$country[$i]</country>
+        <ishere>$isHere[$i]</ishere>
+      </clients>
+    }
+
+  }
 }
